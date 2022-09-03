c5995955afb61e1c3c3052a54eb84c487c5a2a23
evgKireev <bob1_k@mail.ru>
2022-09-03 13:33:54 +0300

    modal window

--- app/Desks.js
+++ app/Desks.js
@@ -1,6 +1,7 @@
 import { API } from './API.js';
 import { $ } from './DOM.js';
 import { User } from './User.js';
+import { Modal } from './Modal.js';
 import {
   FETCHING_ERROR_MESSAGE,
   WHILE_ERROR_MOVING,
@@ -17,6 +18,7 @@ import {
   boardTodoStateProgress,
   boardTodoStateDone,
   deleteAll,
+  root,
 } from './elements.js';
 
 export class Desks extends User {
@@ -25,6 +27,9 @@ export class Desks extends User {
   }
 
   applyContent(el, templete, board) {
+    boardTodoState.el.textContent = this.getDesks.create.length;
+    boardTodoStateProgress.el.textContent = this.getDesks.progress.length;
+    boardTodoStateDone.el.textContent = this.getDesks.done.length;
     const title = templete.find('.board__card-text');
     const desc = templete.find('.board__descriptions-title');
     const date = templete.find('.board__info-data');
@@ -41,22 +46,27 @@ export class Desks extends User {
     let { create, progress, done } = this.getDesks;
     if (this.getDesks.create.length) {
       create.forEach((el) => {
-        boardTodoState.el.textContent = this.getDesks.create.length;
         const createBoardTodo = $(
           document.importNode(boardTodo.el.content, true)
         );
         const btnMove = createBoardTodo.find('.board__descriptions-move');
         btnMove.addEvent('click', () => {
-          const create = this.getDesks.create.filter(
-            (todo) => todo.id !== el.id
-          );
-          const progress = [...this.getDesks.progress, el];
-          const newDesks = { ...this.getDesks, create, progress };
-          this.fetcher(
-            () => API.putUser(this.getUserId, { desks: newDesks }),
-            this.appendDesk.bind(this),
-            WHILE_ERROR_MOVING
-          );
+          const limit = 4;
+          if (this.getDesks.progress.length >= limit) {
+            Modal.showModalLimit()
+            return
+          } else {
+            const create = this.getDesks.create.filter(
+              (todo) => todo.id !== el.id
+            );
+            const progress = [...this.getDesks.progress, el];
+            const newDesks = { ...this.getDesks, create, progress };
+            this.fetcher(
+              () => API.putUser(this.getUserId, { desks: newDesks }),
+              this.appendDesk.bind(this),
+              WHILE_ERROR_MOVING
+            );
+          }
         });
 
         const btnDelete = createBoardTodo.find('.board__card-delete');
@@ -81,7 +91,6 @@ export class Desks extends User {
     boardDesc.clear();
     if (this.getDesks.progress.length) {
       progress.forEach((el) => {
-        boardTodoStateProgress.el.textContent = this.getDesks.progress.length;
         const createBoardProgress = $(
           document.importNode(boardTodoProgress.el.content, true)
         );
@@ -134,19 +143,9 @@ export class Desks extends User {
     boardDone.clear();
     if (this.getDesks.done.length) {
       done.forEach((el) => {
-        boardTodoStateDone.el.textContent = this.getDesks.done.length;
         const createBoardDone = $(
           document.importNode(boardTodoDone.el.content, true)
         );
-
-        deleteAll.el.addEventListener('click', () => {
-          const newDesk = { ...this.getDesks, done: [] };
-          this.fetcher(
-            () => API.putUser(this.getUserId, { desks: newDesk }),
-            this.appendDesk.bind(this),
-            FETCHING_ERROR_MESSAGE
-          );
-        });
         const btnDelete = createBoardDone.find('.board__card-delete');
         btnDelete.addEvent('click', () => {
           const done = this.getDesks.done.filter((todo) => todo.id !== el.id);
@@ -170,5 +169,16 @@ export class Desks extends User {
       this.appendDesk.bind(this),
       FETCHING_ERROR_MESSAGE
     );
+    deleteAll.el.addEventListener('click', () => {
+      const removeAll = () => {
+        const newDesk = { ...this.getDesks, done: [] };
+        this.fetcher(
+          () => API.putUser(this.getUserId, { desks: newDesk }),
+          this.appendDesk.bind(this),
+          WHILE_ERROR_REMOVING
+        );
+      };
+      Modal.showModalRemove(removeAll);
+    });
   }
 }
--- app/Modal.js
+++ app/Modal.js
@@ -1,9 +1,58 @@
+import { DOM } from './DOM.js';
 import { root } from './elements.js';
+import { Desks } from './Desks.js';
 export class Modal {
- static showModal(message) {
+  static showModal(message) {
     root.insertHTML(
       'afterend',
       `<div class="modal show-modal"><h3 class = "form__modal-text">${message}</h3></div>`
     );
+  }
+  static showModalRemove(collback) {
+    const removeElement = DOM.create('div', 'modal', 'show-modal');
+    root.el.append(removeElement.el);
+    removeElement.insertHTML(
+      'afterbegin',
+      `<div class="modal-remove">
+     <h3 class="modal-remove-title">Are you sure you want to delete?</h3>
+      <div class="modal-remove-inner">
+      <button class="modal-remove-cancel">Cancel</button>
+      <button class="modal-remove-confirm">Confirm</button></div>
+    </div>`
+    );
+
+    const cancelBtn = removeElement.find('.modal-remove-cancel');
+    cancelBtn.addEvent('click', () => {
+      removeElement.remove();
+    });
+
+    const confirmBtn = removeElement.find('.modal-remove-confirm');
+    confirmBtn.addEvent('click', () => {
+      collback() || removeElement.remove();
+    });
+  }
+  static showModalLimit() {
+    const removeElement = DOM.create('div', 'modal', 'show-modal');
+    root.el.append(removeElement.el);
+    removeElement.insertHTML(
+      'afterbegin',
+      `<div class="modal-remove">
+     <h3 class="modal-remove-title">
+     Please complete the current tasks, and then move on to the new ones!</h3>
+      <div class="modal-remove-inner">
+      <button class="modal-remove-cancel">Cancel</button>
+      <button class="modal-remove-confirm">Confirm</button></div>
+    </div>`
+    );
+
+    const cancelBtn = removeElement.find('.modal-remove-cancel');
+    cancelBtn.addEvent('click', () => {
+      removeElement.remove();
+    });
+
+    const confirmBtn = removeElement.find('.modal-remove-confirm');
+    confirmBtn.addEvent('click', () => {
+      removeElement.remove();
+    });
   }
 }
